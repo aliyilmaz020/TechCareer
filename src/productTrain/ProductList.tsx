be4788b0cbe336db605a77639e4666dae4008511
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-08-13 18:54:51 +0300

    Update ProductList.tsx

--- src/productTrain/ProductList.tsx
+++ src/productTrain/ProductList.tsx
@@ -15,8 +15,8 @@ const ProductList = () => {
     <View>
       <FlatList
         data={products}
-        renderItem={({ item }) => (item.unitPrice > 50) ? <Text style={{ backgroundColor: 'red' }}>{item.name} - {item.unitPrice}</Text> :
-          <Text>{item.name} - {item.unitPrice}</Text>} />
+        renderItem={({ item }) => (item.unitPrice > 50) ? <Text style={{ backgroundColor: 'red', fontSize: 20, fontWeight: '400' }}>{item.name} - {item.unitPrice}</Text> :
+          <Text style={{ fontSize: 20, fontWeight: '400' }}>{item.name} - {item.unitPrice}</Text>} />
     </View>
   );
 };
